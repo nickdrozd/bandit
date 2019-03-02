7606c34fd4229b51c6015b0e4918b4cd855e36de
Nick Drozd <nicholasdrozd@gmail.com>
2019-03-02 09:28:27 -0600

    yappi

--- bandit/__main__.py
+++ bandit/__main__.py
@@ -1,3 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from bandit.cli import main
-main.main()
+import yappi
+
+yappi.set_clock_type('cpu')
+yappi.start()
+
+try:
+    main.main()
+except:
+    print('getting stats')
+    stats = yappi.get_func_stats()
+    stats.save('yappi.callgrind', type='callgrind')
+    print('done')
